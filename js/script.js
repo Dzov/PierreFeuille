baeff0972966774e6c5661b76731c1d090ff302d
Dzov <amelie.haladjian@gmail.com>
2017-03-04 13:08:49 +0100

    keeping score

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
 var choices = ["pierre", "papier", "ciseaux", "lézard", "spock"]; 
 var humanBtn = document.querySelectorAll(".btn"); 
-var resultat = document.querySelector(".resultat"); 
+var resultat = document.querySelector(".resultat");
+var choix = document.querySelector(".choix"); 
+var humanScoreDisplay = document.querySelector("#myCounter"); 
+var robotScoreDisplay = document.querySelector("#robotCounter"); 
+var robotScore = 0; 
+var humanScore = 0; 
+
+humanScoreDisplay.innerHTML = humanScore; 
+robotScoreDisplay.innerHTML = robotScore; 
 
 for (var i = 0 ; i < humanBtn.length ; i++) {
   humanBtn[i].addEventListener("click", function() {
@@ -9,52 +17,60 @@ for (var i = 0 ; i < humanBtn.length ; i++) {
 
     if (humanChoice == robotChoice) {
       resultat.innerHTML = "Egalité!";
-    } 
-    else {
-      if (humanChoice == "ciseaux" && (robotChoice == "papier" || robotChoice == "lézard")) {
-        if(robotChoice == "papier") {
-          resultat.innerHTML = "Gagné! Vos " + humanChoice + " coupent le " + robotChoice + " du robot"; 
-        } else { 
-          resultat.innerHTML = "Gagné! Vos " + humanChoice + " décapitent le " + robotChoice + " du robot"; 
+      choix.innerHTML = ""; 
+    } else {
+        resultat.innerHTML = "Gagné !";
+        if (humanChoice == "ciseaux" && robotChoice == "papier") {
+          humanScore ++; 
+          humanScoreDisplay.innerHTML = humanScore; 
+          choix.innerHTML = "Les ciseaux coupent le papier du robot"; 
+        } else if (humanChoice == "ciseaux" && robotChoice == "lézard") {
+          humanScore ++; 
+          humanScoreDisplay.innerHTML = humanScore; 
+          choix.innerHTML = "Les ciseaux décapitent le lézard du robot"; 
+        } 
+        else if (humanChoice == "papier" && robotChoice == "pierre") {
+          humanScore ++; 
+          humanScoreDisplay.innerHTML = humanScore; 
+          choix.innerHTML = "Le papier bat la pierre du robot"; 
+        } else if (humanChoice == "papier" && robotChoice == "spock") {
+          humanScore ++; 
+          humanScoreDisplay.innerHTML = humanScore; 
+          choix.innerHTML = "Le papier repousse le spock du robot"; 
+        } 
+        else if (humanChoice == "pierre" && robotChoice == "lézard") {
+          humanScore ++; 
+          humanScoreDisplay.innerHTML = humanScore; 
+          choix.innerHTML = "La pierre écrase le lézard du robot"; 
+        } else if (humanChoice == "pierre" && robotChoice == "ciseaux") {
+          humanScore ++; 
+          humanScoreDisplay.innerHTML = humanScore; 
+          choix.innerHTML = "La pierre bat les ciseaux du robot"; 
+        } 
+        else if (humanChoice == "lézard" && robotChoice == "spock") {
+          humanScore ++; 
+          humanScoreDisplay.innerHTML = humanScore; 
+          choix.innerHTML = "Le lézard empoisonne le spock du robot"; 
+        } else if (humanChoice == "lézard" && robotChoice == "papier") {
+          humanScore ++; 
+          humanScoreDisplay.innerHTML = humanScore; 
+          choix.innerHTML = "Le lézard mange le papier du robot"; 
         }
-      } else if (humanChoice == "papier" && (robotChoice == "pierre" || robotChoice == "Spock")) {
-        if(robotChoice == "pierre") {
-          resultat.innerHTML = "Gagné! Votre " + humanChoice + " bat la " + robotChoice + " du robot"; 
-        } else { 
-          resultat.innerHTML = "Gagné! Votre " + humanChoice + " repousse le " + robotChoice + " du robot"; 
+        else if (humanChoice == "spock" && robotChoice == "ciseaux") {
+          humanScore ++; 
+          humanScoreDisplay.innerHTML = humanScore; 
+          choix.innerHTML = "Spock écrabouille les ciseaux du robot"; 
+        } else if (humanChoice == "spock" && robotChoice == "pierre") {
+          humanScore ++; 
+          humanScoreDisplay.innerHTML = humanScore; 
+          choix.innerHTML = "Spock détruit la pierre du robot"; 
+        } 
+        else {
+          robotScore ++; 
+          robotScoreDisplay.innerHTML = robotScore; 
+          resultat.innerHTML = "Perdu !";
+          choix.innerHTML = "Le robot a choisi " + robotChoice + ". Il bat votre " + humanChoice;
         }
-      } else if (humanChoice == "pierre" && (robotChoice == "lézard" || robotChoice == "ciseaux")) {
-        if(robotChoice == "lézard") {
-          resultat.innerHTML = "Gagné! Votre " + humanChoice + " écrase le " + robotChoice + " du robot"; 
-        } else { 
-          resultat.innerHTML = "Gagné! Votre " + humanChoice + " bat les " + robotChoice + " du robot"; 
-        }
-      } else if (humanChoice == "lézard" && (robotChoice == "spock" || robotChoice == "papier")) {
-        if(robotChoice == "spock") {
-          resultat.innerHTML = "Gagné! Votre " + humanChoice + " empoisonne le " + robotChoice + " du robot"; 
-        } else { 
-          resultat.innerHTML = "Gagné! Votre " + humanChoice + " mange le " + robotChoice + " du robot"; 
-        }
-      } else if (humanChoice == "spock" && (robotChoice == "ciseaux" || robotChoice == "pierre")) {
-        if(robotChoice == "ciseaux") {
-          resultat.innerHTML = "Gagné! Votre " + humanChoice + " écrabouille les " + robotChoice + " du robot"; 
-        } else { 
-          resultat.innerHTML = "Gagné! Votre " + humanChoice + " détruit la " + robotChoice + " du robot"; 
-        }
-      } else {
-        resultat.innerHTML = "Perdu! Le " + robotChoice + " du robot bat votre " + humanChoice;
-      }
     } 
   }); 
 } 
-
-// Les ciseaux coupent le papier,
-// le papier bat la pierre,
-// la pierre écrase le lézard,
-// le lézard empoisonne Spock.
-// Spock écrabouille les ciseaux,
-// les ciseaux décapitent le lézard,
-// le lézard mange le papier,
-// le papier repousse Spock,
-// Spock détruit la pierre.
-// La pierre bat les ciseaux
